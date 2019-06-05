13fa91319f7b19eceb13e98649710c5e7d08a8d5
Eddy <quvdong@gmail.com>
2019-06-05 23:45:11 +0900

    add install , weekly update and reboot autostart

--- ipw_update_arm64.sh
+++ ipw_update_arm64.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl --version || apt-get install -y curl
+curl --version > /dev/null 2>&1 || apt-get install -y curl
 
 export REMOTE_VER=$(curl https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/version | cut -f3 -d' ')
 
@@ -10,16 +10,35 @@ export LOCAL_BIN=/usr/local/bin/ipws
 
 export TMP_BIN=/tmp/update-bin
 
-if [ "$(echo $REMOTE_VER $LOCAL_VER | tr " " "\n" | sort -V | head -n 1)" != "$REMOTE_VER" ]
+if which ipws > /dev/null 2>&1
   then
-    curl -o $TMP_BIN https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/$REMOTE_VER/arm64/ipws
-    
-    # Stop IPWS
-    $LOCAL_BIN shutdown >/dev/null 2>&1
+    if [ "$(echo $REMOTE_VER $LOCAL_VER | tr " " "\n" | sort -V | head -n 1)" != "$REMOTE_VER" ]
+      then
+        curl -o $TMP_BIN https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/$REMOTE_VER/arm64/ipws
 
-    mv $TMP_BIN $LOCAL_BIN
+        # Stop IPWS
+        $LOCAL_BIN shutdown >/dev/null 2>&1
+
+        mv $TMP_BIN $LOCAL_BIN
+        chmod +x $LOCAL_BIN
+
+        # Start IPWS
+        $LOCAL_BIN daemon >/dev/null 2>&1 &
+    fi
+  else
+    curl -o $LOCAL_BIN https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/$REMOTE_VER/arm64/ipws
     chmod +x $LOCAL_BIN
-    
+
     # Start IPWS
     $LOCAL_BIN daemon >/dev/null 2>&1 &
 fi
+
+# weekly auto update
+command_update=`basename "$0"`
+job_update="@weekly bash $PWD/$command"
+cat <(fgrep -i -v "$command_update" <(crontab -l)) <(echo "$job_update") | crontab -
+
+# reboot start
+command_reboot=`basename "$0"`
+job_reboot="@reboot $LOCAL_BIN daemon >/dev/null 2>&1 &"
+cat <(fgrep -i -v "$command_reboot" <(crontab -l)) <(echo "$job_reboot") | crontab -
