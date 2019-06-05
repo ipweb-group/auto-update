f92f34343c12a7a342be215c126d1803fc77c895
YanYQ <yanyq@noahsi.com>
2019-06-05 19:10:00 +0900

    fix update

--- ipw_update_x86.sh
+++ ipw_update_x86.sh
@@ -12,11 +12,14 @@ export TMP_BIN=/tmp/update-bin
 
 if [ "$(echo $REMOTE_VER $LOCAL_VER | tr " " "\n" | sort -V | head -n 1)" != "$REMOTE_VER" ]
   then
-    curl -o https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/$REMOTE_VER/x86/ipws
+    curl -o $TMP_BIN https://ipweb-download.oss-ap-northeast-1.aliyuncs.com/$REMOTE_VER/x86/ipws
     
     # Stop IPWS
-    /usr/local/bin/ipws shutdown >/dev/null 2>&1
+    $LOCAL_BIN shutdown >/dev/null 2>&1
+
     mv $TMP_BIN $LOCAL_BIN
+    chmod +x $LOCAL_BIN
+    
     # Start IPWS
-    /usr/local/bin/ipws daemon >/dev/null 2>&1 &
+    $LOCAL_BIN daemon >/dev/null 2>&1 &
 fi
